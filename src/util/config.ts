d4608c49e01d08c2fa9f6004780cb1f7473bc970
Edward <a50541853@gmail.com>
2025-05-26 14:09:55 +0800

    fix

--- src/util/config.ts
+++ src/util/config.ts
@@ -665,6 +665,30 @@ export const option2 = {
       saveAsImage: {},
     },
   },
+	dataZoom: [
+		{
+			type: 'inside'
+		},
+		{
+			type: 'slider',
+			showDataShadow: false,
+			handleIcon:
+				'path://M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+			handleSize: '80%'
+		},
+		{
+			type: 'inside',
+			orient: 'vertical'
+		},
+		{
+			type: 'slider',
+			orient: 'vertical',
+			showDataShadow: false,
+			handleIcon:
+				'path://M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+			handleSize: '80%'
+		}
+	],
 	// Legend to distinguish between Horsepower and Torque lines
 	legend: {
 			data: ['马力', '扭矩'],
